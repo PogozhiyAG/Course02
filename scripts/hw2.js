04546c455b5d3ac2fb309f11af37f3e0262d97a9
aleksandr.pogozhii <aleksandr.pogozhii@devexpress.com>
2023-10-13 17:16:01 +0100

    const

--- scripts/hw2.js
+++ scripts/hw2.js
@@ -6,31 +6,31 @@ function task1(){
 }
 
 function task2(){
-    let year = 2007;
+    const year = 2007;
     alert(year);
 }
 
 function task3(){
-    let creator = 'Brendan Eich';
+    const creator = 'Brendan Eich';
     alert(creator);
 }
 
 function task4(){
-    let x = 10;
-    let y = 2;
+    const x = 10;
+    const y = 2;
     let message = `Сумма: ${x + y}\nРазность: ${x - y}\nПроизведение: ${x * y}\nЧастное: ${x / y}`;
     alert(message);
 }
 
 function task5(){
-    let result = Math.pow(2, 5);    
+    const result = Math.pow(2, 5);    
     alert(result);
 }
 
 function task6(){
-    let a = 9;    
-    let b = 2;
-    let result = a % b
+    const a = 9;    
+    const b = 2;
+    const result = a % b
     alert(result);
 }
 
@@ -63,7 +63,7 @@ function task8(){
 
 function task9(){
     //9.0
-    let user = {
+    const user = {
         name: 'John',
         age: 25,
         isAdmin: false
@@ -79,8 +79,8 @@ function task9(){
     delete user['city of residence'];
 
     //9.4
-    let propName = prompt('Какую информацию хотите узнать о пользователе?');
-    let propValue = user[propName];
+    const propName = prompt('Какую информацию хотите узнать о пользователе?');
+    const propValue = user[propName];
     if(propValue !== undefined){
         alert(propValue);
     }
@@ -90,6 +90,6 @@ function task9(){
 }
 
 function task10(){
-    let name = prompt('Как вас зовут?');
+    const name = prompt('Как вас зовут?');
     alert(`Привет, ${name}!`);
 }
